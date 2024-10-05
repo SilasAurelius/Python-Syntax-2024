bdaa5019df5aab9e7a08fe62323e25a1bceacd8c
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-10-05 19:45:00 -0400

    Update Python-Naming-Convention.py
    Pi value and max limit are both constants so I changed the names to UPPERCASE.

--- Python-Naming-Convention.py
+++ Python-Naming-Convention.py
@@ -1,10 +1,10 @@
 # October 3, 2024
 # Task 1: Code Correction You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
 
-pi_value = 3.14
+PI_VALUE = 3.14
 user_age = 25
 user_location = "New York"
-max_limit = 1000
+MAX_LIMIT = 1000
 
 
 
